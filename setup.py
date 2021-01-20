8d8cae2f68f7fd0234dcfb2c86a29878cac52985
Thomas MATHIEU <thomas.mathieu@m4x.org>
2021-01-20 10:57:07 +0100

    packaging the code in an installable hand-made-neural-network package

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name='hand_made_neural_network',
+      description="Personal deep learning implementation inspired by keras",
+      packages=["hand_made_neural_network"])
