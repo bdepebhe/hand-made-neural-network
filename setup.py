ea89ca7aec53c48293c8bdf1d9d685faae8934d0
Thomas MATHIEU <thomas.mathieu@m4x.org>
2021-02-14 12:32:24 +0100

    renaming

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
-from setuptools import setup
 
-setup(name='hand_made_neural_network',
-      description="Personal deep learning implementation inspired by keras",
-      packages=["hand_made_neural_network"])
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='hmnn',
+    version='0.0.1',
+    author='Thomas Mathieu',
+    author_email='thomas.mathieu@m4x.org',
+    description='Personal deep learning implementation inspired by keras',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    #url='https://github.com/bdepebhe/handmade-neural-network.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'matplotlib>=3.3.2',
+        'numpy>=1.18.4',
+        'pandas>=1.1.4',
+        'scikit-learn>=0.23.2',
+        'seaborn>=0.11.0',
+        'tensorflow==2.3.1',
+    ]
+)
